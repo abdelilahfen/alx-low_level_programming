fed1b26491a3f12af4486ca19804a49fecbd3063
abdelilah fen <abdelilahfen@gmail.com>
2022-12-13 16:29:03 -0800

    table times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 			{
 				if (b == 0)
 				{
-					_putchar('0')
+					_putchar('0');
 				}
 
 				if (b != 0)
@@ -32,13 +32,13 @@ void times_table(void)
 			}
 			else
 			{
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 
